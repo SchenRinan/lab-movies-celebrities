ea335fd7eb047a4e761919a97833cf477b0a763e
Rinan <jandrewschnell@gmail.com>
2023-06-03 16:31:53 +0200

    day 0 iteration 2 done

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,10 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+// all your routes here
+router.get("/celebrities", (req, res, next) => {
+    res.render("celebrities/celebrities");
+  });
+
+
+module.exports = router;
